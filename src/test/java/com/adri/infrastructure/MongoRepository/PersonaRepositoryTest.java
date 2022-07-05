dacac920a2cacc0244765871715e5357b2750847
Adrián Tena Burgos <adrian.tena@bosonit.com>
2022-07-05 11:52:50 +0200

    initial

---
+++ src/test/java/com/adri/infrastructure/MongoRepository/PersonaRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.adri.infrastructure.MongoRepository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaRepositoryTest {
+
+    @Test
+    void findByName() {
+    }
+}
